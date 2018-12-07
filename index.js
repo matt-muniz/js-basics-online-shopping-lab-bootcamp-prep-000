7f82ffadbe5d9fa57e369de1f82ffab79eef7b21
matthew t muniz-silva <mmunizsi01@gmail.com>
2018-12-07 01:58:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var cart = [];
+var newObject = {}
 
 function getCart() {
  return cart;
@@ -11,12 +12,31 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
-}
+  var price = Math.floor((Math.random() * 100) + 1);
+  newObject = {itemName: item, itemPrice: price}
+  cart.push(newObject)
 
+  if(cart.length > 0){
+    return `${newObject.itemName} has been added to your cart.`
+  }else{
+    return "Your shopping cart is empty."
+  }
+}
+addToCart("mocha")
 function viewCart() {
+  
   // write your code here
+  if (cart.length > 0){
+    for(var i = 0; i < cart.length; i++){
+      cart.push(newObject[i])
+    }
+    return `In your cart, you have ${newObject.itemName} at $${newObject.itemPrice}.`
+  }else{
+    return "Your shopping cart is empty."
+  }
+  
 }
-
+viewCart()
 function total() {
   // write your code here
 }
